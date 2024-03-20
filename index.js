ec9edd62595fab7fab2a7e1acbdc6c9a2099dda2
andmarmuk <163561594+andmarmuk@users.noreply.github.com>
2024-03-20 14:06:02 +0200

    Update index.js add dots

--- index.js
+++ index.js
@@ -10,7 +10,26 @@ let ekran = document.getElementById('ekran');
 //display to screen
   for(item of buttons){item.addEventListener('click', function () {
     a = this.value;
-    ekran.innerText += a;});}
+    // a !== '.'
+    a !== '=';
+    if ((a == '+' || a == 'MOD' || a == '-' || a == '*' || a == '/') & x == '' ){
+    ekran.innerText = '';
+    a = '';
+    console.log(a)
+} else if (a == '.' & (y !=''|| x != '')& !x.includes('.')) {
+    ekran.innerText += a;
+    console.log(a);
+} else if (a == '.' & (y !=''|| x != '')& y > '' & !y.includes('.')) {
+    ekran.innerText += a;
+    console.log(a);
+} else if ((a == '+' || a == 'MOD' || a == '-' || a == '*' || a == '/' ) & x > '' & operator == '' ){
+    ekran.innerText += a;
+    console.log(a);
+} else if (a !== '+' & a !== 'MOD' & a !== '-' & a !== '*' & a !== '/' & a !== operator & a!== '.') { 
+    ekran.innerText += a;
+    console.log(a);}
+})}
+
 //clean display
 document.getElementById('C').addEventListener("click", function () {
     ekran.innerText = '';
@@ -20,10 +39,21 @@ document.getElementById('C').addEventListener("click", function () {
 })
 //numbers:x
 for (item of numbers){ item.addEventListener('click', function(){
-    if (ekran.innerText >= a, operator =='' ){
+    if (x >= '' & operator =='' & a != '.'){
         x += a;
+        console.log(x);}
+    if (x > '' & operator ==''& a == '.' ){
+        x += a;
+        console.log(x);}
+    if (ekran.innerText == ''& operator ==''& a =='.'  ){
+        x != a;
+        y != a;
+        ekran.innerText = ''
+        console.log(x);}
+    if (y ==''& a =='.'& x != '' & operator != '' ){
+        y != a;
         console.log(x);
-    } else if (operator!=''){
+    } else if (operator > '' ){
         y += a;
         console.log(y);
     }});}
@@ -44,19 +74,29 @@ for (item of operators){ item.addEventListener('click', function(){
 });}
 //calculate result
 document.getElementById('row').addEventListener("click", function () {
-    if(operator == '+'){
+    if(operator == '+'& x !=='' & y !== ''){
         ekran.innerText = (parseFloat)(x) + (parseFloat)(y);
         console.log(ekran.innerText);
-    } else if (operator == '-'){
+    } else if (operator == '-'& x !=='' & y !== ''){
         ekran.innerText = (parseFloat)(x) - (parseFloat)(y);
-    } else if (operator == '*'){
+    } else if (operator == '*'& x !=='' & y !== ''){
         ekran.innerText = (parseFloat)(x) * (parseFloat)(y);
-    } else if (operator == '/'){
+    } else if (operator == '/'& x !=='' & y !== '' & y !== '0'){
         ekran.innerText = (parseFloat)(x) / (parseFloat)(y);
-    } else if (operator == 'MOD'){
+    } else if (operator == 'MOD'& x !=='' & y !== ''){
         ekran.innerText = (parseFloat)(x) % (parseFloat)(y);
         console.log(ekran.innerText);
-    }});
+    } else if (operator == '') {ekran.innerText = ''
+} else if (x =='' || y == '' || x == '=' || y == '=') {ekran.innerText = ''
+} else if (operator == '/'& y == '0'){
+    ekran.innerText = 'Choose another number';
+} 
+    x =  ekran.innerText;
+    console.log(x);
+    y = '';
+    operator = '';
+    console.log(y);
+});
 //change color
 document.getElementById('color').addEventListener("click", function () {
 theme = document.querySelector('div.all');
